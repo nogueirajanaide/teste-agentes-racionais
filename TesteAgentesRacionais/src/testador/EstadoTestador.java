aec390c9c8baf59d572f0859907532e844ffc007
raquel.vsilveira <raquel.vsilveira@hotmail.com>
2013-01-03 14:29:46 +0000

    Inclusao dos arquivos no repositorio

---
+++ TesteAgentesRacionais/src/testador/EstadoTestador.java
@@ -0,0 +1,51 @@
+package testador;
+
+public class EstadoTestador {
+	
+	//Parâmetro de entrada do teste, um inteiro que identifica a interação inicial entre o agente testado e seu ambiente 
+	private int T;
+	
+	//Parâmetro de entrada do teste, um inteiro que identifica a interação final entre o agente testado e o agente que representa seu ambiente 
+	private int N;
+	
+	//Identificação do programa Agente
+	private final String AGENTE = "Ag";
+	
+	//Identificação do programa Ambiente
+	private final String AMBIENTE = "Amb";
+	
+	//Um conjunto de casos de teste
+	private Populacao CasosTeste;
+
+	public int getT() {
+		return T;
+	}
+
+	public void setT(int t) {
+		T = t;
+	}
+
+	public int getN() {
+		return N;
+	}
+
+	public void setN(int n) {
+		N = n;
+	}
+
+	public Populacao getCasosTeste() {
+		return CasosTeste;
+	}
+
+	public void setCasosTeste(Populacao casosTeste) {
+		CasosTeste = casosTeste;
+	}
+
+	public String getAGENTE() {
+		return AGENTE;
+	}
+
+	public String getAMBIENTE() {
+		return AMBIENTE;
+	}
+}
---
+++ TesteAgentesRacionais/src/testador/FuncaoAvaliacao.java
@@ -0,0 +1,33 @@
+package testador;
+
+import agente.Acao;
+
+public class FuncaoAvaliacao {
+	private int elemento;
+	private String acao;
+	private int pontuacao;
+	
+	public int getElemento() {
+		return elemento;
+	}
+	
+	public void setElemento(int elemento) {
+		this.elemento = elemento;
+	}
+	
+	public String getAcao() {
+		return acao;
+	}
+	
+	public void setAcao(String acao) {
+		this.acao = acao;
+	}
+	
+	public int getPontuacao() {
+		return pontuacao;
+	}
+	
+	public void setPontuacao(int pontuacao) {
+		this.pontuacao = pontuacao;
+	}
+}
